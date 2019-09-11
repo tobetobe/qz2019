c0dd3e21cd79cd06c0e3f52e4b2e2999e327897f
tobetobe <949043032@qq.com>
2019-09-12 00:02:18 +0800

    call学习

--- CallApplyBind/index.js
+++ CallApplyBind/index.js
@@ -2,7 +2,8 @@ var xw = {
   name: '小王',
   gender: '男',
   age: 24,
-  say: function () {
+  say: function (...args) {
+    console.log('now this', this, args)
     console.log(this.name + ' , ' + this.gender + ' ,今年' + this.age)
   }
 }
@@ -11,10 +12,13 @@ var xh = {
   gender: '女',
   age: 18
 }
+// xw.say()
+
+// xw.say.call(xh)
+
+// xw.say.apply(xh)
+ 
+// xw.say.bind(xh)()
+
 xw.say()
-
-xw.say.call(xh)
-
-xw.say.apply(xh)
-
-xw.say.bind(xh)()
+xw.say.call(xh, 1, 2)
