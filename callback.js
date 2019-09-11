2ee7f67580bd1148413ec2bbdf4ff9ff78c00567
tobetobe <949043032@qq.com>
2019-09-12 01:22:58 +0800

    自己实现一个callback

--- callback.js
+++ callback.js
@@ -7,8 +7,22 @@ const fa = (a, fb) => {
   }
 }
 
-const cb = (x) => {
+const cb = x => {
   console.log('callback', x)
 }
 
-fa(10, cb)(x)
+// fa(10, cb)(x)
+
+let Event = {}
+
+Event.on = function (signal, callback) {
+  if (signal == 'event01') {
+    console.log(signal)
+    console.log(typeof callback)
+    callback()
+}
+}
+
+Event.on('event01', () => {
+  console.log('callback')
+})
